84e71b402d9bfc22419bd1bbad0b55c19d85bda8
sanjukumari-tech <foodlazzez@gmail.com>
2024-09-24 20:50:30 +0530

    this is country app

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 
 const app = express();
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 9000;
 const db_url = process.env.MONGO_URI;
 
 app.use(cors());
